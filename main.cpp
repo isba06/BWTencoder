7236aed2d3a00de75bf76b4c4c87aa2612fbf726
Ismail <www.ismail.com@mail.ru>
2022-02-04 23:48:21 +0300

    ass bwt(Burrows–Wheeler transform)

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 
-string shiftLine(string&& line){
+string shiftLine(string& line){
     char last = line.back();
     for(int i = line.size(); i > 0; i--){
         line[i] = line[i-1];
@@ -13,7 +14,34 @@ string shiftLine(string&& line){
     line[0] = last;
     return line;
 }
+void printVector(vector<string> &a){
+    for(const auto& item : a){
+        cout << item << endl;
+    }
+}
+pair<string, int> bwt(string& line){
+    vector<string> arrayLines;
+    arrayLines.reserve(line.size());
+    for(int i = 0; i < line.size(); i++) {
+        arrayLines.push_back(shiftLine(line));
+    }
+    sort(arrayLines.begin(), arrayLines.end());
+    cout << "sorted: " << endl;
+    int pos = -1, buffPos;
+    string lineOut;
+    for ( auto item : arrayLines){
+        pos++;
+        if(line == item)
+            buffPos = pos;
+        lineOut += item.back();
+    }
+    printVector(arrayLines);
+    pair<string, int> out;
+    out = make_pair(lineOut, buffPos);
+    cout << out.first << " " << out.second;
+    return out;
 
+}
 int main() {
     ifstream inFile("bib");
     if(inFile.is_open()){
@@ -25,6 +53,7 @@ int main() {
     else{
         cout << "File not open" << endl;
     }
-    cout << shiftLine("asfdgskgs") << endl;
+    string kl = "IsmailBayramov";
+    bwt(kl);
     return 0;
 }
